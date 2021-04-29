e26f7f3f9211fe40000479d7f88925f4cf4e3d1b
Jonathan Dawe <JDawe@esriuk.com>
2021-04-29 13:41:01 +0100

    revert to build

--- README.md
+++
@@ -1,6 +0,0 @@
-# arcgis api and snowpack
-An example of using ArcGIS ES Modules with Snowpack
-
-- [Snowpack Intro](https://www.snowpack.dev/tutorials/getting-started)
-- [reference blog](https://odoe.net/blog/snowpack-jsapi)
-
--- snowpack.config.js
+++
@@ -1,8 +0,0 @@
-// snowpack.config.js
-module.exports = {
-    mount: {
-        "node_modules/@arcgis/core/assets": {url: "/assets", static: true, resolve: false},
-        "src": "/",
-    },
-    plugins: [["@snowpack/plugin-webpack"]]
-};
